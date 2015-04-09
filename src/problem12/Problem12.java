6e51d934dcd7e42c554ee4021aa46fe876b683bf
Arjun Muhunthan <am4012@imperial.ac.uk>
2015-04-09 23:08:30 +0100

    Halfway complete with problem13

--- src/problem12/Problem12.java
+++ src/problem12/Problem12.java
@@ -1,5 +1,44 @@
 package problem12;
 
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+//Answer = 76576500
+
 public class Problem12 {
 
+	public static BigInteger nTriangleNumber(int n) {
+		BigInteger x = new BigInteger(Integer.toString(n)); 
+		BigInteger x1 = new BigInteger(Integer.toString(n + 1)); 
+		BigInteger r = (x.multiply(x1)).divide(new BigInteger(Integer.toString(2))); 
+		return r; 
+	}
+	
+	public static Set<Integer> getDivisors(BigInteger x) {
+		Set<Integer> result = new HashSet<Integer>();
+		result.add(1);
+		result.add(x.intValue());
+		for (int i = 2; i * i < x.intValue(); i++) {
+			if ((x.intValue() % i) == 0) {
+				result.add(i);
+				result.add(x.intValue() / i);
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		int biggestNumDivs = 0;
+		int nTriangle = 0;
+		while (biggestNumDivs < 500) {
+			nTriangle++;
+			BigInteger num = nTriangleNumber(nTriangle);
+			int numDivs = getDivisors(num).size();
+			if (biggestNumDivs < numDivs) {
+				biggestNumDivs = numDivs;
+			}
+		}
+		System.out.println(nTriangleNumber(nTriangle));
+	}
 }
--- src/problem13/Problem13.java
+++ src/problem13/Problem13.java
@@ -1,5 +1,57 @@
 package problem13;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Problem13 {
+
+	static ArrayList<String> numbers = new ArrayList<String>();
+
+	static StringBuilder result = new StringBuilder();
+	
+	public static void importData() throws FileNotFoundException {
+		File file = new File("./src/problem13/data");
+		Scanner sc = new Scanner(file);
+		while (sc.hasNext()) {
+			numbers.add(sc.nextLine());
+		}
+		sc.close();
+	}
+
+	public static int getUnits(int x) {
+		return x % 10;
+	}
+
+	public static int getSD(int x) {
+		return (x - (x % 10)) / 10;
+	}
+
+	public static boolean numbersEmpty() {
+		for (String s : numbers) {
+			if (s.length() != 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		importData();
+		int carry = 0;
+		while (!numbersEmpty()) {
+		for (int i = 0; i < numbers.size(); i++) {
+			carry += numbers.get(i).charAt(numbers.get(i).length()-1) - '0';
+			numbers.set(i, numbers.get(i).substring(0, numbers.get(i).length()-1));
+			
+		}
+		result.append(getUnits(carry));
+		carry = getSD(carry);
+		System.out.println(carry);
+		}
+		System.out.println(result.toString());
+		
+	}
 
 }
