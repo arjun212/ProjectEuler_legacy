afae6c8c4fae9d3de911eb319dbd6be66e02726f
Arjun Muhunthan <am4012@imperial.ac.uk>
2015-04-11 17:15:40 +0100

    Edited commit script some more

--- src/commit.sh
+++ src/commit.sh
@@ -1,9 +1,17 @@
 
-git add .
-if [ -n "$1" ]
-then
-	git commit -m "$1"
-	git push
-else
-	echo "Please add a Git Message"
-fi
+git status
+read -r -p "Would you like to continue (y/n)" inp
+
+case $inp in
+	[yY])
+		git add .
+		if [ -n "$1" ]
+		then
+			git commit -m "$1"
+			git push
+		else
+			echo "Please add a Git Message"
+		fi
+		;;
+	*)
+esac
